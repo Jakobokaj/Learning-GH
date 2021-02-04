13caabd1ee6fb5b0fe6f31ad00d617e033a08045
Jakob Strand <jakobstrand2001@gmail.com>
2021-02-04 13:20:06 +0100

    Initial Commit

---
+++ Learning GitHub/Learning_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GitHubApp.swift
+//  Learning GitHub
+//
+//  Created by Jakob Strand on 04/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
